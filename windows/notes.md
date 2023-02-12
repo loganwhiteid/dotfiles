2df17a166c49bca6a4c08ccbb02adf255310e5c4
Logan Whiteid <logan@whiteid.dev>
2023-02-12 10:01:31 -0800

    Powershell profile updates

---
+++ windows/notes.md
@@ -0,0 +1,7 @@
+## NOTES
+
+### Powershell
+- profile: %userprofile%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+### terminal
+- settings: %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
---
+++ windows/powershell/PROFILE/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+﻿## Powershell Profile
+
+
+# list directory after CD'ing to it
+function cl {cd $args[0]; ls}
+
+function tail ($file) {
+Get-Content $file -Wait
+}
+
+function vim {nvim $args}
+
+function neovim {nvim $args}
+
+function repos {
+    $loc = "D:\repos\"
+    if ($args.Count -gt 0){
+        $loc = Join-Path -Path $loc -ChildPath $args[0]
+    }
+    cl $loc
+}
