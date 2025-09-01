65095e3c90dc62ef69ffc4f2913ce0990ac625c1
Logan Whiteid <logan@whiteid.dev>
2025-09-01 06:42:12 -0700

    more chat config

--- nvim/lua/custom/plugins/chat.lua
+++ nvim/lua/custom/plugins/chat.lua
@@ -21,8 +21,8 @@ return {
         f:close()
         return api_key
       end,
-      google = function()
-        local f = assert(io.open(os.getenv("HOME") .. "/.chat/google", "r"))
+      openrouter = function()
+        local f = assert(io.open(os.getenv("HOME") .. "/.chat/openrouter", "r"))
         local api_key = string.gsub(f:read("*all"), "\n", "")
         f:close()
         return api_key
@@ -35,11 +35,31 @@ return {
       },
       anthropic = {
         ["claude4"] = "claude-sonnet-4-20250514",
+        ["haiku"] = "claude-3-5-haiku-20241022",
+      },
+      openrouter = {
+        ["or_cl4"] = "anthropic/claude-sonnet-4",
+        ["or_haiku"] = "anthropic/claude-3.5-haiku",
+        ["or_grok_code"] = "x-ai/grok-code-fast-1",
+        ["or_grok4"] = "x-ai/grok-4",
+        ["or_gpt5"] = "openai/gpt-5",
+        ["or_gpt5nano"] = "openai/gpt-5-nano",
+        ["or_gpt5mini"] = "openai/gpt-5-mini",
+        ["or_geminipro"] = "google/gemini-2.5-pro",
+        ["or_geminiflash"] = "google/gemini-2.5-flash",
       },
     },
     default = {
-      model = "gpt-4.1",
+      model = "or_cl4",
+      temp = 0.4,
+      system_message = [[You are a senior software engineer and best practices are very important. Your colleague will ask you various questions about their code and ask you to assist with some coding tasks. 
+Answer concisely and when asked for code avoid unnecessary verbose explanation. Only provide usage and explanation when asked.]],
     },
-    title_model = "gpt-4.1-nano",
+    title_model = "or_haiku",
+  },
+  keys = {
+    { "<leader>cf", "<cmd>ChatFocus<cr>", desc = "focus" },
+    { "<leader>cn", "<cmd>ChatNew<cr>", desc = "new chat" },
+    { "<leader>co", "<cmd>ChatOpen<cr>", desc = "open chat" },
   },
 }
